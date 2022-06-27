abc63c9fc7588c88a0f488a4199822040d6947f5
David <dhalls@cmnhospitals.org>
2022-06-27 15:09:57 -0600

    Update QuizQuestion.js

--- src/QuizQuestion.js
+++ src/QuizQuestion.js
@@ -3,15 +3,18 @@ import QuizQuestionButton from './QuizQuestionButton.js'
 
 class QuizQuestion extends Component{     
     render(){
+        const num= this.props.quiz_question.answer_options
         return (
             <main>
         <section>
           <p>{this.props.quiz_question.instruction_text}</p>
         </section>
         <section className="buttons">
-            
           <ul>
-            <QuizQuestionButton button_text={this.props.quiz_question.answer_options[0]} />
+            {                
+                num.map((n) => <QuizQuestionButton button_text={n} key={n.toString()} value={n} />)
+            }         
+            
           </ul>
         </section>
       </main>
